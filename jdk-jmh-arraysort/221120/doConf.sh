0f781a2e2e1a1d446fc7bddf27973effdbe3f3bb
bourgesl <bourges.laurent@gmail.com>
2022-11-20 13:49:17 +0100

    new DPQS 22.11 results vs opendjk20 master

---
+++ jdk-jmh-arraysort/221120/doConf.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+source ~/test-jdk19.sh
+
+export CC=gcc-10
+export CXX=g++-10
+
+# 4 ie no HT
+JOBS=4
+
+# -Ofast or -O3
+# --with-boot-jdk='${JDK}'
+
+# extra flags:  -funroll-loops -fomit-frame-pointer
+FLAGS='-O3 -march=native -mtune=skylake'
+
+bash configure \
+               --with-jobs=$JOBS \
+               --with-extra-cflags="${FLAGS}" \
+               --with-extra-cxxflags="${FLAGS}" \
+               --with-jmh=build/jmh/jars
+
